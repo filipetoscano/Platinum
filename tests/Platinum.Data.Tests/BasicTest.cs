1c9e349fcf4ddb507861111196a30bf106946d6a
Filipe Toscano <ft@itm.net>
2016-05-18 20:32:04 +0200

    + Add: Placeholder assemblies for .Data assembly.

---
+++ tests/Platinum.Data.Tests/BasicTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Platinum.Data.Tests
+{
+    [TestClass]
+    public class BasicTest
+    {
+        [TestMethod]
+        public void Method1()
+        {
+        }
+    }
+}
---
+++ tests/Platinum.Data.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// Assembly information
+[assembly: AssemblyTitle( "" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Platinum" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: CLSCompliant( true )]
+[assembly: ComVisible( false )]
+[assembly: Guid( "39e1e4ca-bb63-484d-b736-a08b7a83bad2" )]
+[assembly: NeutralResourcesLanguage( "en-GB" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
+
+/* eof */
