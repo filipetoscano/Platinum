5b1653d55305e399c666e33c30d43bd27a8f1069
Filipe Toscano <ft@itm.net>
2016-08-15 10:19:18 +0200

    + Add: Path.Combine doesn't canonicalize paths, and Path.GetFullPath hits the filesystem when the path is local :/

---
+++ src/Platinum.Core/IO/FsPath.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+
+namespace Platinum.IO
+{
+    public static class FsPath
+    {
+        /// <summary>
+        /// Canonicalizes the path, removing any relative navigations.
+        /// </summary>
+        /// <param name="path">
+        /// Path to be canonicalized.
+        /// </param>
+        /// <returns>
+        /// Canonicalized path.
+        /// </returns>
+        public static string Canonicalize( string path )
+        {
+            #region Validations
+
+            if ( path == null )
+                throw new ArgumentNullException( nameof( path ) );
+
+            #endregion
+
+            string originalRoot = string.Empty;
+
+            if ( Path.IsPathRooted( path ) == true )
+            {
+                originalRoot = Path.GetPathRoot( path );
+                path = path.Substring( originalRoot.Length );
+            }
+
+            string fakeRoot = @"\\fake\root\";
+            path = Path.Combine( fakeRoot, path );
+            path = Path.GetFullPath( path );
+
+            path = path.Substring( fakeRoot.Length );
+            path = Path.Combine( originalRoot, path );
+
+            return path;
+        }
+
+
+        /// <summary>
+        /// Combines an array of strings into a path.
+        /// </summary>
+        /// <param name="paths">
+        /// An array of parts of the path.
+        /// </param>
+        /// <returns>
+        /// The combined paths, canonicalized.
+        /// </returns>
+        public static string Combine( params string[] paths )
+        {
+            string c = Path.Combine( paths );
+
+            return Canonicalize( c );
+        }
+
+
+        /// <summary>
+        /// Combines two strings into a path.
+        /// </summary>
+        /// <param name="path1">
+        /// The first path to combine.
+        /// </param>
+        /// <param name="path2">
+        /// The second path to combine.
+        /// </param>
+        /// <returns>
+        /// The combined paths, canonicalized. If one of the specified paths is a
+        /// zero-length string, this method returns the other path. If path2
+        /// contains an absolute path, this method returns path2.
+        /// </returns>
+        public static string Combine( string path1, string path2 )
+        {
+            string c = Path.Combine( path1, path2 );
+
+            return Canonicalize( c );
+        }
+
+
+        /// <summary>
+        /// Combines three strings into a path.
+        /// </summary>
+        /// <param name="path1">
+        /// The first path to combine.
+        /// </param>
+        /// <param name="path2">
+        /// The second path to combine.
+        /// </param>
+        /// <param name="path3">
+        /// The third path to combine.
+        /// </param>
+        /// <returns>
+        /// The combined paths, canonicalized.
+        /// </returns>
+        public static string Combine( string path1, string path2, string path3 )
+        {
+            string c = Path.Combine( path1, path2, path3 );
+
+            return Canonicalize( c );
+        }
+
+
+        /// <summary>
+        /// Combines four strings into a path.
+        /// </summary>
+        /// <param name="path1">
+        /// The first path to combine.
+        /// </param>
+        /// <param name="path2">
+        /// The second path to combine.
+        /// </param>
+        /// <param name="path3">
+        /// The third path to combine.
+        /// </param>
+        /// <param name="path4">
+        /// The fourth parth to combine.
+        /// </param>
+        /// <returns>
+        /// The combined paths, canonicalized.
+        /// </returns>
+        public static string Combine( string path1, string path2, string path3, string path4 )
+        {
+            string c = Path.Combine( path1, path2, path3, path4 );
+
+            return Canonicalize( c );
+        }
+
+
+        /// <summary>
+        /// Creates a uniquely named, zero-byte temporary file on disk and returns the full
+        /// path of that file.
+        /// </summary>
+        /// <returns>
+        /// The full path of the temporary file.
+        /// </returns>
+        public static string GetTempFileName()
+        {
+            return Path.GetTempFileName();
+        }
+
+
+        /// <summary>
+        /// Returns the path of the current user's temporary folder.
+        /// </summary>
+        /// <returns>
+        /// The path to the temporary folder, ending with a backslash.
+        /// </returns>
+        public static string GetTempPath()
+        {
+            return Path.GetTempPath();
+        }
+    }
+}
---
+++ tests/Platinum.Core.Tests/IO/FsPathTest.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Platinum.IO;
+
+namespace Platinum.Core.Tests.IO
+{
+    [TestClass]
+    public class FsPathTest
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            string x = FsPath.Canonicalize( "C:/foo" );
+
+            Assert.AreEqual( @"C:\foo", x );
+        }
+
+
+        [TestMethod]
+        public void Test02()
+        {
+            string x = FsPath.Canonicalize( "C:/foo/bar" );
+
+            Assert.AreEqual( @"C:\foo\bar", x );
+        }
+
+
+        [TestMethod]
+        public void Test03()
+        {
+            string x = FsPath.Canonicalize( "C:/foo/../bar" );
+
+            Assert.AreEqual( @"C:\bar", x );
+        }
+
+
+        [TestMethod]
+        public void Test04()
+        {
+            string x = FsPath.Canonicalize( "C:/foo/./bar" );
+
+            Assert.AreEqual( @"C:\foo\bar", x );
+        }
+
+
+        [TestMethod]
+        public void Test05()
+        {
+            string x = FsPath.Canonicalize( "C:/foo/bar/.." );
+
+            Assert.AreEqual( @"C:\foo", x );
+        }
+
+
+        [TestMethod]
+        public void Test06()
+        {
+            string x = FsPath.Canonicalize( @"C:\foo" );
+
+            Assert.AreEqual( @"C:\foo", x );
+        }
+
+
+        [TestMethod]
+        public void Test07()
+        {
+            string x = FsPath.Canonicalize( @"C:\foo\bar" );
+
+            Assert.AreEqual( @"C:\foo\bar", x );
+        }
+
+
+        [TestMethod]
+        public void Test08()
+        {
+            string x = FsPath.Canonicalize( @"C:\foo\..\bar" );
+
+            Assert.AreEqual( @"C:\bar", x );
+        }
+
+
+        [TestMethod]
+        public void Test09()
+        {
+            string x = FsPath.Canonicalize( @"C:\foo\.\bar" );
+
+            Assert.AreEqual( @"C:\foo\bar", x );
+        }
+
+
+        [TestMethod]
+        public void Test10()
+        {
+            string x = FsPath.Canonicalize( @"C:\foo\bar\.." );
+
+            Assert.AreEqual( @"C:\foo", x );
+        }
+
+
+        [TestMethod]
+        public void Test11()
+        {
+            string x = FsPath.Canonicalize( @"C:\..\foo\bar\.." );
+
+            Assert.AreEqual( @"C:\foo", x );
+        }
+
+
+        [TestMethod]
+        public void TestAll()
+        {
+            string x = FsPath.Canonicalize( @"C:/foo\./bar\../foo" );
+
+            Assert.AreEqual( @"C:\foo\foo", x );
+        }
+    }
+}
