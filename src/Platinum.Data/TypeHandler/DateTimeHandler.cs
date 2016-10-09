e9efe0723e1959664c0ab2ffed172561cbe3bdd7
Filipe Toscano <ft@itm.net>
2016-10-09 20:44:44 +0200

    + Add: Database read out from SQL Server don't have DateTimeKind. Enforce UTC.

---
+++ src/Platinum.Data/TypeHandler/DateTimeHandler.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using System;
+using System.Data;
+
+namespace Platinum.Data.TypeHandler
+{
+    /// <summary>
+    /// Ensures that all dates read from the database, are automatically
+    /// read as UTC.
+    /// </summary>
+    public class DateTimeHandler : SqlMapper.TypeHandler<DateTime>
+    {
+        /// <summary>
+        /// Assign the value of a parameter before a command executes.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter to configure.
+        /// </param>
+        /// <param name="value">
+        /// Parameter value.
+        /// </param>
+        public override void SetValue( IDbDataParameter parameter, DateTime value )
+        {
+            parameter.Value = value;
+        }
+
+
+        /// <summary>
+        /// Parse a database value back to a typed value.
+        /// </summary>
+        /// <param name="value">
+        /// The value from the database.
+        /// </param>
+        /// <returns>
+        /// The typed value.
+        /// </returns>
+        public override DateTime Parse( object value )
+        {
+            return DateTime.SpecifyKind( (DateTime) value, DateTimeKind.Utc );
+        }
+    }
+}
