8ac9a48e1fe7b3e80067b6250daa6b7cacc9850f
Filipe Toscano <ft@itm.net>
2017-01-19 10:03:42 +0100

    + Add: async method overloads, for our own extensions. + Add: When reading dates through Dapper, always ensure that the DateTime.Kind is Utc. Since, by default, all date+time should be in UTC this works well as compliant infrastructure. If yours is not the case, don't call the built-in DataConfig.Register.

---
+++ src/Platinum.Data/DataConfig.cs
@@ -0,0 +1,15 @@
+﻿using Dapper;
+using Platinum.Data.TypeHandler;
+
+namespace Platinum.Data
+{
+    /// <summary />
+    public static class DataConfig
+    {
+        /// <summary />
+        public static void Register()
+        {
+            SqlMapper.AddTypeHandler( new DateTimeHandler() );
+        }
+    }
+}
--- src/Platinum.Data/Extensions.cs
+++ src/Platinum.Data/Extensions.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Platinum.Data
 {
@@ -33,6 +34,22 @@ namespace Platinum.Data
         /// </summary>
         /// <typeparam name="T">Enumerate type.</typeparam>
         /// <param name="cnn">Connection.</param>
+        /// <param name="command">Command.</param>
+        /// <returns>Value of first selected cell, as enumerate.</returns>
+        public async static Task<T> ExecuteEnumAsync<T>( this DbConnection cnn, CommandDefinition command )
+        {
+            string scalar = await cnn.ExecuteScalarAsync<string>( command );
+
+            return Enumerate.Parse<T>( scalar );
+        }
+
+
+        /// <summary>
+        /// Execute parameterized SQL that selects a single string value,
+        /// and converts into enumerate value.
+        /// </summary>
+        /// <typeparam name="T">Enumerate type.</typeparam>
+        /// <param name="cnn">Connection.</param>
         /// <param name="sql">SQL statement/command.</param>
         /// <param name="param">Parameters.</param>
         /// <param name="transaction">Whether command participates in transaction.</param>
@@ -42,6 +59,26 @@ namespace Platinum.Data
         public static T ExecuteEnum<T>( this IDbConnection cnn, string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default( int? ), CommandType? commandType = default( CommandType? ) )
         {
             string scalar = cnn.ExecuteScalar<string>( sql, param, transaction, commandTimeout, commandType );
+
+            return Enumerate.Parse<T>( scalar );
+        }
+
+
+        /// <summary>
+        /// Execute parameterized SQL that selects a single string value,
+        /// and converts into enumerate value.
+        /// </summary>
+        /// <typeparam name="T">Enumerate type.</typeparam>
+        /// <param name="cnn">Connection.</param>
+        /// <param name="sql">SQL statement/command.</param>
+        /// <param name="param">Parameters.</param>
+        /// <param name="transaction">Whether command participates in transaction.</param>
+        /// <param name="commandTimeout">Command timeout.</param>
+        /// <param name="commandType">Type of command.</param>
+        /// <returns>Value of first selected cell, as enumerate.</returns>
+        public static async Task<T> ExecuteEnumAsync<T>( this IDbConnection cnn, string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = default( int? ), CommandType? commandType = default( CommandType? ) )
+        {
+            string scalar = await cnn.ExecuteScalarAsync<string>( sql, param, transaction, commandTimeout, commandType );
 
             return Enumerate.Parse<T>( scalar );
         }
