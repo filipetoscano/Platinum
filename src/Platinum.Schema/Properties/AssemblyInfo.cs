a26acd31d17a21f8b64f9a7cd0def498d821358c
Filipe Toscano <ft@itm.net>
2017-02-18 00:15:34 +0100

    + Add: Nuget for the Platinum schema files. + Add: Reference the XSD from the XML, so that we get auto-complete inside Visual Studio. * Change: Use the new tools from Platinum, rather than from Yttrium.

---
+++ src/Platinum.Schema/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "Platinum Schema" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Platinum" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "139b339d-cd0a-4ea8-883d-e0e865454a32" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
