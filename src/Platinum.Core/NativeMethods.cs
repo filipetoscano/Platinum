4c5507e04dd18157da325009c5c25bca49a13ea3
Filipe Toscano <filipe.toscano@gmail.com>
2017-05-12 02:30:56 +0200

    Add PreciseDateTime implementation
    The default precision of DateTime is rather limited. Starting with Windows 8 / Windows Server 2012 there's an additional Win32 API that provides a much higher precision, albeit slower.

---
+++ src/Platinum.Core/NativeMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Platinum
+{
+    /// <summary />
+    internal static class NativeMethods
+    {
+        /// <summary>
+        /// Higher precision time.
+        /// </summary>
+        /// <param name="filetime">Timestamp.</param>
+        [DllImport( "Kernel32.dll", CallingConvention = CallingConvention.Winapi, EntryPoint = "GetSystemTimePreciseAsFileTime" )]
+        internal static extern void GetSystemTimePreciseAsFileTime( out long filetime );
+    }
+}
---
+++ src/Platinum.Core/PreciseDateTime.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Platinum
+{
+    /// <summary />
+    public static class PreciseDateTime
+    {
+        private static Lazy<bool> _isPrecise = new Lazy<bool>( () =>
+        {
+            try
+            {
+                PreciseTimeGet();
+                return true;
+            }
+            catch ( EntryPointNotFoundException )
+            {
+                return false;
+            }
+        } );
+
+
+        /// <summary>
+        /// Gets a <see cref="DateTime" /> object that is set to the current date and time on this
+        /// computer, expressed as the Coordinated Universal Time (UTC).
+        /// </summary>
+        public static DateTime UtcNow
+        {
+            get
+            {
+                if ( _isPrecise.Value == false )
+                    return DateTime.UtcNow;
+                else
+                    return PreciseTimeGet();
+            }
+        }
+
+
+        /// <summary>
+        /// Gets whether the timestamps returned by the current class are precise
+        /// or not.
+        /// </summary>
+        public static bool IsPrecise
+        {
+            get { return _isPrecise.Value; }
+        }
+
+
+        /// <summary />
+        private static DateTime PreciseTimeGet()
+        {
+            long preciseTime;
+            NativeMethods.GetSystemTimePreciseAsFileTime( out preciseTime );
+
+            return DateTime.FromFileTimeUtc( preciseTime );
+        }
+    }
+}
